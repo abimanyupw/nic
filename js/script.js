02a775d1d7963d330fca81c36a41240592f61061
abimanyupw <143879625+abimanyupw@users.noreply.github.com>
2025-04-01 12:51:16 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 const navbarnav = document.querySelector('.nav');
 const hm = document.querySelector('#hamburger-menu');
+const navlinks = document.querySelectorAll('.nav a')
+
 // Hamburger menu toggle
 document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarnav.classList.toggle('active');
     e.preventDefault();
 };
-if (!hm.contains(e.target) && !navbarnav.contains(e.target)) {
-    navbarnav.classList.remove('active');
-}
+document.addEventListener('click', (e) => {
+    if (!hm.contains(e.target) && !navbarnav.contains(e.target)) {
+        navbarnav.classList.remove('active');
+    }
+});
+
+navlinks.forEach((link) => {
+    link.addEventListener('click',() => {
+        navbarnav.classList.remove('active')
+    })
+})
